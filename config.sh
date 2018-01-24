4561730fc39b1a14fce5fdc4f23811148b833bc8
Bingco <ittat@live.com>
2018-01-24 10:20:41 +0800

    Update config.sh

--- config.sh
+++ config.sh
@@ -31,7 +31,7 @@ case `uname` in
 esac
 
 GITREPO=${GITREPO:-"git://github.com/cubieboard2-b2g/b2g-manifest"}
-BRANCH=${BRANCH:-cb2-4.2}
+BRANCH=${BRANCH:-"cb2-4.2"}
 
 while [ $# -ge 1 ]; do
 	case $1 in
